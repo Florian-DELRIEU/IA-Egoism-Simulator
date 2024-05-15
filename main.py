e32199a9be07c5788db7438ea5033cb71ef2bfdc
Florian DELRIEU <florian.delrieu@free.fr>
2024-05-15 20:26:24 +0200

    [v0.1.3] - move contents in main file

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-import window
+import matplotlib.pyplot as plt
+from window import generate_window, plot_item
+from classes import Character2D
 
+generate_window()
+
+C1 = Character2D(0,0,1,0)
+plt.pause(1)
+plot_item(C1)
+plt.show()
+
+for i in range(10):
+    plt.pause(1)
+    C1.move(1)
--- window.py
+++ window.py
@@ -1,6 +1,10 @@
 import matplotlib.pyplot as plt
 
-plt.figure("main window",figsize=(20,10))
-plt.xlim(-10,10)
-plt.ylim(-10,10)
-plt.show()
+def plot_item(item):
+    plt.plot(item.x,item.y,item.color)
+
+def generate_window():
+    plt.figure("main window",figsize=(20,10))
+    plt.xlim(-10,10)
+    plt.ylim(-10,10)
+    plt.show()
